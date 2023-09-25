e2b48bb7ed7814548d5818195c5798973903146a
Willian Marchesan <willian.marchesan@gmail.com>
2023-09-24 22:23:59 -0300

    Doc: Adicionado Log

--- src/main/java/br/com/will/service/impl/NumberServiceImpl.java
+++ src/main/java/br/com/will/service/impl/NumberServiceImpl.java
@@ -3,12 +3,14 @@ package br.com.will.service.impl;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
+import br.com.will.annotation.LogMe;
 import br.com.will.service.NumberService;
 import jakarta.enterprise.context.Dependent;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.enterprise.inject.Produces;
 
 @RequestScoped
+@LogMe
 public class NumberServiceImpl implements NumberService {
 
     AtomicLong nextLong = new AtomicLong();
